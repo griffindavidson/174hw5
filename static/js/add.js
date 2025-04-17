ce36c7299730263beccbf43f14d7682a31ee64e2
griffindavidson <69946180+griffindavidson@users.noreply.github.com>
2025-04-16 23:20:37 -0700

    started adding POST /companies

--- static/js/add.js
+++ static/js/add.js
@@ -1,10 +1,66 @@
+const submitButton = document.getElementById('submit');
+
 function addAddress() {
     const container = document.getElementById("addresses");
-    const field = document.createElement("input");
-    
-    field.setAttribute("type", "text");
-    field.setAttribute("name", "address");
-    field.setAttribute("placeholder", "123 Park Way, Atlanta, Georgia, U.S.");
 
-    container.appendChild(field);
-  }
+    const addressField = document.createElement("input");
+    addressField.setAttribute("type", "text");
+    addressField.setAttribute("name", "address");
+    addressField.setAttribute("placeholder", "123 Park Way, Atlanta, Georgia, U.S.");
+
+    const descriptionField = document.createElement("input");
+    descriptionField.setAttribute("type", "text");
+    descriptionField.setAttribute("name", "address");
+    descriptionField.setAttribute("placeholder", "Description of Hub");
+
+    container.appendChild(addressField);
+    container.appendChild(descriptionField);
+}
+
+submitButton.addEventListener("click", () => {
+
+    // grab all inputs into constants
+    const name = document.getElementById('name').value;
+    const services = document.getElementById('services').value;
+    const hubs = Array.from(document.querySelectorAll('input[name="address"]'))
+        .map(input => input.value)
+        .filter(value => value.trim() !== '');
+    const revenue = document.getElementById('revenue').value;
+    const homepage = document.getElementById('homepage').value;
+    const file = document.getElementById('logo');
+    const logo = file.files[0];
+
+    // create JavaScript Object
+    const row = {
+        Company: name,
+        Services: services,
+        Hubs: {
+            Hub: hubs
+        },
+        Revenue: "$" + revenue,
+        HomePage: homepage,
+        Logo: logo.name
+    }
+
+    // create request and turn JS Object into JSON
+    const request = new Request("/companies", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(row),
+        logo: logo // probably should fix this
+    });
+
+    // query flask and await response
+    fetch('/companies')
+        .then(response => response.json())
+        .then(data => {
+            // do something with response
+
+
+        })
+        .catch(error => console.error("Error: ", error));
+
+    
+});
